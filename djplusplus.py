558077d756690c82dff66b0b9c3a25f5294acf86
Saif Azmi <saif.azmi0108@gmail.com>
2016-10-02 11:00:59 +0100

    updated front-end

--- djplusplus.py
+++ djplusplus.py
@@ -7,15 +7,6 @@ def index():
 	return render_template('index.html')
 
 
-@app.route('/submit', methods=['POST'])
-def submit():
-	if request.method == 'POST':
-		 # change to our backend
-		 # redirect to some page
-	else:
-		return abort(405) #method not allowed
-
-
 if __name__ == '__main__':
 	app.debug = True
 	app.run()
