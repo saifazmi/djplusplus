0a07efbd4c7afa8754a54606960360490782ee6c
rrama <ben_durrans@hotmail.co.uk>
2016-10-02 05:23:22 +0100

    Songs by key. Kyle and Tom's stuff.

--- analytics.py
+++ analytics.py
@@ -6,7 +6,7 @@ from os.path import expanduser
 from random import shuffle, randint
 from pydub import AudioSegment, playback
 
-AUTHORIZATION = "Bearer BQBFthaRkE89gu4zE6429YQiJ-rHG6-F_JYQh_snV1LT57mSTWBR_MrmysyM3wVw5VD8hrX9dyHYomJHYknme94WlVBTBZLr9qfFfh4rdGeSeDJ-daez3ZDglYJNypQzZByBSIpKV6RE"
+AUTHORIZATION = "Bearer BQAQbYWzeyAqhYoSDCAOS8o2F3k-2dHHB5zEL7G5EYkEXD2033cEU00a96A-XHecd9cX8WPL8s1CcY1rB_XCqDdPGC8WJmgNf3v2Ja5q1MvfM3NvuKo-qviIZQwqlCmacs1BuzFJxxzX"
 MUSIC_FOLDER = expanduser("~") + "/Music"
 
 
@@ -34,42 +34,73 @@ def get_analytics(track_id):
     return send_request("audio-analysis/" + track_id)
 
 
-def get_sections(query):
-    track_id = get_track_id(query)
-    if track_id is None:
-        return None
-    analytics = get_analytics(track_id)
+def get_key(analytics):
+    key = analytics["track"]["key"]
+    return key
+
+
+def get_sections(analytics):
+    # returns all of the sections of the song from the spotify api
     sections = analytics["sections"]
-    sections = map(lambda section: dict(start=int(1000*section["start"]), duration=int(1000*section["duration"])), sections)
+    sections = map(lambda section: dict(start=int(1000*section["start"]), duration=int(1000*section["duration"])), sections) # pls explain
     return sections
 
 
-def do_file(f):
-    sections = get_sections(f)
+def do_file(f, analytics):
+    sections = get_sections(analytics)
     if sections is None:
         return None
     # sections = [{'duration': 17090, 'start': 0}, {'duration': 21356, 'start': 17090}, {'duration': 54395, 'start': 38446}, {'duration': 7199, 'start': 92842}, {'duration': 68318, 'start': 100041}, {'duration': 11558, 'start': 168359}, {'duration': 46519, 'start': 179918}, {'duration': 61686, 'start': 226438}, {'duration': 8007, 'start': 288124}]
     print sections
-    chosen = randint(0, len(sections) - 1)
+    chosen = randint(0, len(sections) - 1)  # choose an index randomly to select a
     print sections[chosen]
     full_f = MUSIC_FOLDER + "/" + f
     song = AudioSegment.from_mp3(full_f)
     song = song[sections[chosen]["start"]:]
-    song = song[0:sections[chosen]["duration"]]
-    playback.play(song)
+    song = song[0:sections[chosen]["duration"]] # filter out the single section
+    return song
 
 
 def read_music():
     global MUSIC_FOLDER
-    files = listdir(MUSIC_FOLDER)
-    shuffle(files)
+    files = listdir(MUSIC_FOLDER)  # gives a list of all the files in the music folder
+    shuffle(files)  # shuffles the files within the list
+    songs_by_key = {}
+    i = 0
     for f in files:
         if not re.match(".*\\.mp3", f):
             continue
         # f = "Eminem - Kings Never Die.mp3"
         print f
-        do_file(f)
-        return
+        track_id = get_track_id(f)
+        if track_id is None:
+            continue
+        analytics = get_analytics(track_id)
+        key = get_key(analytics)
+        song = do_file(f, analytics)
+        if str(key) not in songs_by_key.keys():
+            songs_by_key[str(key)] = []
+            print songs_by_key[str(key)]
+        songs_by_key[str(key)].append(song)
+        
+        i += 1
+        if i == 10:
+            break
+    
+    key = 0
+    while True:
+        key = randint(0, len(songs_by_key) - 1)
+        if str(key) in songs_by_key.keys():
+            break
+    
+    out = None
+    for song in songs_by_key[str(key)]:
+        if out is None:
+            out = song
+        else:
+            out *= song
+    playback.play(out)
+
 
 if __name__ == '__main__':
     try:
